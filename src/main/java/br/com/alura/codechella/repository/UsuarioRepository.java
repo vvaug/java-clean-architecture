cbee275145b45a407803355d112e36874629a01e
vvaugusto <victorhyuuk1@gmail.com>
2024-06-04 20:09:08 -0300

    feature/migrate-to-mongo

--- src/main/java/br/com/alura/codechella/repository/UsuarioRepository.java
+++ src/main/java/br/com/alura/codechella/repository/UsuarioRepository.java
@@ -1,7 +1,7 @@
 package br.com.alura.codechella.repository;
 
 import br.com.alura.codechella.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends MongoRepository<Usuario, String> {
 }
